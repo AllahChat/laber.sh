9a0a475581e2ff38b7ac9afda1e8a6fc01424225
AllahChat <126810196+AllahChat@users.noreply.github.com>
2023-05-06 21:40:38 +0200

    Add files via upload

--- laber.sh
+++ laber.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-polygen english.grm | sed -e "s/ s /s /g" | sed -e "s/- /-/g" | sed -e "s/ ly /ly /g" | tee /dev/tty | trans -b en:de 2>/dev/null | tee /dev/tty | espeak -s 10 -v mb-de7
+polygen english.grm | sed -e "s/ s /s /g" | sed -e "s/- /-/g" | sed -e "s/ ly /ly /g" | tee /dev/tty | trans en:de 2>/dev/null | tail -n 1 | cut -d "." -f 1  |  tee /dev/tty | espeak -s 10 -v mb-de7
 
 
